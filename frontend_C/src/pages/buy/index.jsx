1aa47216e180bac1de3a2c98363d3e6bc6ed4c27
Blockchain-Key <1416893908@qq.com>
2022-08-10 23:59:09 +0800

    modified

--- frontend_C/src/pages/buy/index.jsx
+++ frontend_C/src/pages/buy/index.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
 import styles from "./index.less"
-
+import React,{useState,useEffect} from 'react';
+import {history} from "umi"
 import Header from "@/components/Header"
 import Brand from "@/components/Brand"
 import Info from "@/components/Info"
 import Button from "@/components/Button"
+import { typeList } from '@/utils/typeList.js';
+import {getAllGames, getGameByGameId, getGameByOwnerAddr, getMintedNFTList, getUserNFTs} from "../../../../flow/scripts"
+import {createGame, createGameNFTTemplate, mintGameNFT, mintThemeNFT} from "../../../../flow/transactions"
 export default function index(props) {
-  const brand = JSON.parse(props.location.query.brand)
-  const number = props.location.query.number
+  const [number,setNumber] = useState()
+  const [game,setGame] = useState({})
+  useEffect(() => {
+    const uid = props.location.query?.uid;
+    const number = props.location.query?.number;
+    if(uid){
+      getGameByGameId(uid).then(item=>{
+        setGame(item)
+      })
+    }
+    if(number){
+      setNumber(number)
+    }
+  },[])
+  const {uid,gameName,timestamp,slogan,templateType,hostAddr} = game
+  const brandInfo = {
+    title: slogan,
+    topic: gameName,
+    type: templateType,
+    img: {backgroundImage:'url(https://s1.ax1x.com/2022/08/07/vKVZuR.jpg)'},
+  };
+  const brand = {
+    title: brandInfo.title,
+    topic: brandInfo.topic,
+    bg: {
+      ...brandInfo.img,
+      ...typeList[brandInfo.type],
+    },
+  };
+  
+  const theme = async ()=>{
+    // await mintThemeNFT(hostAddr,uid,'black')
+    history.push(`/deal?uid=${uid}&number=${number}`);
+
+  }
   return (
     <div>
       <div className={styles.main}>
@@ -20,10 +56,10 @@ export default function index(props) {
           </div>
           <div className={styles.right}>
             <div className="mb-8">
-              <Info></Info>
+            <Info topic={gameName} time={timestamp}></Info>
             </div>
             <div className="flex justify-center">
-              <Button content="Minte" url={`/deal?brand=${JSON.stringify(brand)}&number=${number}`}></Button>
+              <Button content="Mint" mintTheme={number} theme={theme}></Button>
             </div>
           </div>
         </main>
--- frontend_C/src/pages/deal/index.jsx
+++ frontend_C/src/pages/deal/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import styles from "./index.less"
 
 import {user} from "@/utils/user"
@@ -8,10 +8,39 @@ import Brand from "@/components/Brand"
 import Info from "@/components/Info"
 import Button from "@/components/Button"
 import User from "@/components/User"
+import { typeList } from '@/utils/typeList.js';
+import {getAllGames, getGameByGameId, getGameByOwnerAddr, getMintedNFTList, getUserNFTs} from "../../../../flow/scripts"
+import {createGame, createGameNFTTemplate, mintGameNFT, mintThemeNFT} from "../../../../flow/transactions"
 export default function index(props) {
-  console.log(user);
-  const brand = JSON.parse(props.location.query.brand)
-  const number = props.location.query.number
+  const [number,setNumber] = useState()
+  const [game,setGame] = useState({})
+  useEffect(() => {
+    const uid = props.location.query?.uid;
+    const number = props.location.query?.number;
+    if(uid){
+      getGameByGameId(uid).then(item=>{
+        setGame(item)
+      })
+    }
+    if(number){
+      setNumber(number)
+    }
+  },[])
+  const {uid,gameName,timestamp,slogan,templateType,hostAddr} = game
+  const brandInfo = {
+    title: slogan,
+    topic: gameName,
+    type: templateType,
+    img: {backgroundImage:'url(https://s1.ax1x.com/2022/08/07/vKVZuR.jpg)'},
+  };
+  const brand = {
+    title: brandInfo.title,
+    topic: brandInfo.topic,
+    bg: {
+      ...brandInfo.img,
+      ...typeList[brandInfo.type],
+    },
+  };
   return (
     <div>
       <div className={styles.main}>
@@ -32,10 +61,10 @@ export default function index(props) {
           </div>
           <div className={styles.right}>
             <div className="mb-8">
-              <Info></Info>
+              <Info topic={gameName} time={timestamp}></Info>
             </div>
             <div className="flex justify-center mt-14">
-              <Button content="Sign Up Again" ></Button>
+              <Button content="3D-Printed RaceNumber" ></Button>
             </div>
             <div className="flex justify-center mt-14">
                 <Button content="Rent"></Button>
