d2b91cc0b92a1c4179dc8ba921e12113094f0233
Samsara9527 <cnhwvista@gmail.com>
2022-08-10 23:09:48 +0800

    增加 C 端的首页

--- frontend_C/.umirc.ts
+++ frontend_C/.umirc.ts
@@ -5,14 +5,14 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    // {path:'/',component:'@/pages/home'},
-    {path:'/index',component: '@/pages/index'},
+    { path: '/', component: '@/pages/home' },
+    { path: '/index', component: '@/pages/index' },
     { path: '/sign', component: '@/pages/sign' },
     { path: '/choose', component: '@/pages/choose' },
     { path: '/edit', component: '@/pages/edit' },
-    {path: '/buy', component: '@/pages/buy'},
-    {path: '/deal', component: '@/pages/deal'}
+    { path: '/buy', component: '@/pages/buy' },
+    { path: '/deal', component: '@/pages/deal' }
   ],
   fastRefresh: {},
-  mfsu:{}
+  mfsu: {}
 });
