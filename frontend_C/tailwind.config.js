18e787fd39eab5a12f794ed31f6725af0ce079a4
Blockchain-Key <1416893908@qq.com>
2022-08-15 22:36:16 +0800

    B and C modified tailwindcss

--- frontend_C/tailwind.config.js
+++ frontend_C/tailwind.config.js
@@ -1,28 +1,25 @@
 module.exports = {
-    // mode: 'jit',
-    // jit document: https://tailwindcss.com/docs/just-in-time-mode
-    purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      fontFamily: {
-        'EBRIMA': ['EBRIMA']
-      },
-      extend: {
-        spacing: {
-          'main': '1280px',
-        },
-        colors: {
-          'blue': '#0012BF',
-        },
-      },
-      colors:{
-        'card-true':'#001F9E',
-        'card-false':'#D9D9D9',
-        'white':"#ffffff"
-      }
+  mode: "jit",
+  purge: ["./src/**/*.{ts,tsx,js,jsx}"],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      'EBRIMA': ['EBRIMA']
     },
-    variants: {
-      extend: {},
+    extend: {
+      spacing: {
+        'main': '1280px',
+      },
+      colors: {
+        'blue': '#0012BF',
+      },
     },
-    plugins: [],
+    colors:{
+      'card-true':'#001F9E',
+      'card-false':'#D9D9D9',
+      'white':"#ffffff"
+    }
+  },
+  variants: {},
+  plugins: [],
 };
